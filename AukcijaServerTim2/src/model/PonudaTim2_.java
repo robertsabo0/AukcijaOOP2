5f2cf8bb669d2df8c247891af2b58ae5cda27e69
Milan <milan95bt@gmail.com>
2017-06-07 09:56:34 +0200

    Dodat password za korisnika

--- AukcijaServerTim2/src/model/PonudaTim2_.java
+++ AukcijaServerTim2/src/model/PonudaTim2_.java
@@ -1,14 +1,16 @@
 package model;
 
+import java.util.Date;
 import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="Dali", date="2017-06-05T21:57:48.280+0200")
+@Generated(value="Dali", date="2017-06-06T12:33:07.888+0200")
 @StaticMetamodel(PonudaTim2.class)
 public class PonudaTim2_ {
 	public static volatile SingularAttribute<PonudaTim2, Long> id;
 	public static volatile SingularAttribute<PonudaTim2, Double> vrednost;
 	public static volatile SingularAttribute<PonudaTim2, UserTim2> user;
 	public static volatile SingularAttribute<PonudaTim2, StavkaTim2> stavka;
+	public static volatile SingularAttribute<PonudaTim2, Date> timestamp;
 }
--- AukcijaServerTim2/src/model/StavkaTim2_.java
+++ AukcijaServerTim2/src/model/StavkaTim2_.java
@@ -1,16 +1,18 @@
 package model;
 
+import java.util.Date;
 import javax.annotation.Generated;
-import javax.persistence.metamodel.CollectionAttribute;
+import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="Dali", date="2017-06-05T21:57:48.338+0200")
+@Generated(value="Dali", date="2017-06-06T12:33:07.905+0200")
 @StaticMetamodel(StavkaTim2.class)
 public class StavkaTim2_ {
 	public static volatile SingularAttribute<StavkaTim2, Long> id;
 	public static volatile SingularAttribute<StavkaTim2, String> naziv;
 	public static volatile SingularAttribute<StavkaTim2, String> opis;
+	public static volatile SingularAttribute<StavkaTim2, Date> datumPostavljanja;
 	public static volatile SingularAttribute<StavkaTim2, Boolean> prodata;
 	public static volatile SingularAttribute<StavkaTim2, Double> aktuelnaCena;
 	public static volatile SingularAttribute<StavkaTim2, UserTim2> postavljenoOdStrane;
@@ -18,5 +20,5 @@ public class StavkaTim2_ {
 	public static volatile SingularAttribute<StavkaTim2, BojaTim2> boja;
 	public static volatile SingularAttribute<StavkaTim2, VelicinaTim2> velicina;
 	public static volatile SingularAttribute<StavkaTim2, TipTim2> tip;
-	public static volatile CollectionAttribute<StavkaTim2, Object> nudio;
+	public static volatile ListAttribute<StavkaTim2, PonudaTim2> nudio;
 }
