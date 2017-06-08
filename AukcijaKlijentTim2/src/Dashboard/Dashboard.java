957529848a36fa688d2d8fcfd821a81852278599
Robii <robert.sabo0@gmail.com>
2017-06-08 18:58:21 +0200

    no more conficts!

--- AukcijaKlijentTim2/src/Dashboard/Dashboard.java
+++ AukcijaKlijentTim2/src/Dashboard/Dashboard.java
@@ -1,45 +1,20 @@
 package Dashboard;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.FlowLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.Date;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
-import javax.el.MethodExpression;
-import javax.faces.application.NavigationCase;
-import javax.faces.flow.Flow;
-import javax.faces.flow.FlowCallNode;
-import javax.faces.flow.FlowNode;
-import javax.faces.flow.MethodCallNode;
-import javax.faces.flow.Parameter;
-import javax.faces.flow.ReturnNode;
-import javax.faces.flow.SwitchNode;
-import javax.faces.flow.ViewNode;
-import javax.faces.lifecycle.ClientWindow;
-import javax.naming.InitialContext;
 import javax.naming.NamingException;
-import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
 import javax.swing.SwingConstants;
 
 import beans.StavkeGetters;
-import main.Aukcija;
 import main.BeansGetter;
 import model.StavkaTim2;
-import model.UserTim2;
-import prikazStavke.GlavniPanel;
 
 public class Dashboard extends JPanel{
 	
@@ -47,17 +22,6 @@ public class Dashboard extends JPanel{
 	 StavkaTim2 s;
 	 JButton dugme;
 	 StavkeGetters dashStavka;
-	 
-	private StavkeGetters getDashboardStavka() throws NamingException
-	{
-		if (dashStavka == null)
-		{
-			InitialContext ctx = new InitialContext();
-			String name = "ejb:/AukcijaServerTim2//StavkeGettersImpl!" + StavkeGetters.class.getName();
-			dashStavka = (StavkeGetters) ctx.lookup(name);
-		}
-		return dashStavka;
-	}
 	 
 	 public static void main(String[] args) {
 			EventQueue.invokeLater(new Runnable() {
@@ -76,9 +40,6 @@ public class Dashboard extends JPanel{
 				}
 			});
 		}
-	 
-	 
-	 
 	 
 	 public Dashboard() throws NamingException{
 		 this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
