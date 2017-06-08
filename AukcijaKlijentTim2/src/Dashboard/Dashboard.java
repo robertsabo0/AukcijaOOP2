ee1f706a0e55bd301801afbbba34100d474f7141
Stefan <simeunovics47@gmail.com>
2017-06-08 19:14:12 +0200

    izmena stavke1

--- AukcijaKlijentTim2/src/Dashboard/Dashboard.java
+++ AukcijaKlijentTim2/src/Dashboard/Dashboard.java
@@ -81,7 +81,6 @@ public class Dashboard extends JPanel{
 	 
 	 
 	 public Dashboard() throws NamingException{
-<<<<<<< HEAD
 		 this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
 		 
@@ -93,22 +92,11 @@ public class Dashboard extends JPanel{
 		 
 		 
 		 JLabel licitiraneL=new JLabel("Licitirane stavke: ");
-		 licitiraneL.setHorizontalAlignment(SwingConstants.CENTER);
+		 licitiraneL.setHorizontalAlignment(BoxLayout.Y_AXIS);
 		 add(licitiraneL);
 		 JPanel flow1=new JPanel();
 		 flow1.setLayout(new FlowLayout());
 		 add(flow1);
-=======
-		 this.setLayout(new FlowLayout());
-		 
-		 List<StavkaTim2> licitirane=getDashboardStavka().licitiraneStavke("admin");
-		 List<StavkaTim2> postavljene=getDashboardStavka().postavljeneStavke("admin");
-		 List<StavkaTim2> stavke= BeansGetter.stavkeGetters().getSve();
-		 
-		 JScrollPane skrol=new JScrollPane();
-		 JLabel l1=new JLabel("Licitirane stavke: ");
-		 add(l1);
->>>>>>> b135350337015a92b6a014f7d8d51144ecd638fb
 		 for(int i=0;i<licitirane.size();i++){
 			 JPanel stavka=new JPanel();
 			 stavka.setLayout(new BoxLayout(stavka, BoxLayout.Y_AXIS));
